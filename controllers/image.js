fdb78fdc70477c3c8b8b2da8afa3ba9b3a9fe34e
prakharrrajput007 <prakharrrajput007@gmail.com>
2022-05-19 05:36:04 +0530

    minor

---
+++ controllers/image.js
@@ -0,0 +1,14 @@
+handleImagecounts = (req, res , db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err=>res.status(400).json('unable to find'))
+}
+
+module.exports = {
+    handleImagecounts: handleImagecounts
+}
