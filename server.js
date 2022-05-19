adc3ecab69aebbbd85ac6defcbbeb47b03c77404
prakharrrajput007 <prakharrrajput007@gmail.com>
2022-05-19 18:06:12 +0530

    ':('

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : 5432 ,
-    user : 'postgres',
-    password : 'prakhar',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    // port : 5432 ,
+    // user : 'postgres',
+    // password : 'prakhar',
+    // database : 'smart-brain'
+    ssl: true
   }
 });
 
